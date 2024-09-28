43f3adfc7ae12f880aa6387fabd5c86354f6d5cc
sohini8328 <sohini.tomar@gmail.com>
2024-09-27 20:55:26 -0500

    updated histogram

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 import streamlit as st
 import altair as alt
 import plotly.express as px
+import pandas as pd
+import numpy as np
+
 
 
 st.header('Market data for used cars')
 st.write('Filter cars with under 80,000 miles on it')
+
+df = pd.read_csv('vehicles_us.csv')
+
+df['model_year'] = df['model_year'].fillna(0)
+df['cylinders'] = df['cylinders'].fillna(0)
+df['paint_color']=df['paint_color'].fillna('unknown')
+df['is_4wd'] = df['is_4wd'].fillna(0)
+
+
+car_model = df['model'].unique()
+
+selected_model = st.selectbox('Select a car model', car_model)
+
+
+
+
+
