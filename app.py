6e5a5b83cfd39882dfc3b1dad7fd8cf712d002c5
sohini8328 <sohini.tomar@gmail.com>
2024-09-27 21:38:20 -0500

    updated year

--- app.py
+++ app.py
@@ -20,6 +20,13 @@ car_model = df['model'].unique()
 
 selected_model = st.selectbox('Select a car model', car_model)
 
+min_year, max_year = int(df['model_year'].min()), int(df['model_year'].max())
+
+year_range = st.slider("Choose years", value=(min_year, max_year), min_value= min_year, max_value= max_year)
+                                                
+actual_range = list(range(year_range[0], year_range[1]+1))
+
+
 
 
 
